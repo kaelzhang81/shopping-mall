bfb43743a4e6032d8e40b10775adf64e015f5032
kaelzhang81 <zhangy1981@qq.com>
2016-06-28 23:33:05 +0800

    admin add item to order

--- src/usercases/usecases.go
+++ src/usercases/usecases.go
@@ -102,5 +102,42 @@ type AdminOrderInteractor struct {
 }
 
 func (interactor *AdminOrderInteractor) Add(userId, orderId, itemId int) error {
+	var message string
+	user := interactor.UserRepository.FindById(userId)
+	order := interactor.OrderRepository.FindById(orderId)
+	if !user.IsAdmin {
+		message = "User #%i (customer #%i) "
+		message += "is not allowed to add items "
+		message += "to order #%i (of customer #%i) "
+		message += "because he is not an administrator"
+		err := fmt.Errorf(message,
+			user.Id,
+			user.Customer.Id,
+			order.Id,
+			order.Customer.Id)
+		interactor.Logger.Log(err.Error())
+		return err
+	}
+
+	item := interactor.ItemRepository.FindById(itemId)
+	if domainErr := order.Add(item); domainErr != nil {
+		message = "Could not add Item #%i "
+		message += "to order #%i (of customer #%i) "
+		message += "as user #%i because a bussiness "
+		message += "rule was violated: '%s'"
+		err := fmt.Errorf(message,
+			item.Id,
+			order.Id,
+			order.Customer.Id,
+			user.Id,
+			domainErr.Error())
+		interactor.Logger.Log(err.Error())
+		return err
+	}
+	interactor.OrderRepository.Store(order)
+	interactor.Logger.Log(fmt.Sprintf("Admin added item '%s' (#%i) to order #%i",
+		item.Name,
+		item.Id,
+		order.Id))
 	return nil
 }

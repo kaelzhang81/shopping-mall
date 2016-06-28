758e1ed10cb6a113e9b8643000c134c0c2c858a2
kaelzhang81 <zhangy1981@qq.com>
2016-06-28 23:23:37 +0800

    add item to order

--- src/usercases/usecases.go
+++ src/usercases/usecases.go
@@ -38,7 +38,7 @@ func (interactor *OrderInteractor) Items(userId, orderId int) ([]Item, error) {
 	user := interactor.UserRepository.FindById(userId)
 	order := interactor.OrderRepository.FindById(orderId)
 	if user.Customer.Id != order.Customer.Id {
-		message := "User #%i (customer #%) "
+		message := "User #%i (customer #%i) "
 		message += "is not allowed to see items "
 		message += "in order #%i (of customer #%i)"
 		err := fmt.Errorf(message,
@@ -58,6 +58,42 @@ func (interactor *OrderInteractor) Items(userId, orderId int) ([]Item, error) {
 }
 
 func (interactor *OrderInteractor) Add(userId, orderId, itemId int) error {
+	var message string
+	user := interactor.UserRepository.FindById(userId)
+	order := interactor.OrderRepository.FindById(orderId)
+	if user.Customer.Id != order.Customer.Id {
+		message = "User #%i (customer #%i) "
+		message += "is not allowed to add items "
+		message += "to order #%i (of customer #%i)"
+		err := fmt.Errorf(message,
+			user.Id,
+			user.Customer.Id,
+			order.Id,
+			order.Customer.Id)
+		interactor.Logger.Log(err.Error())
+		return err
+	}
+
+	item := interactor.ItemRepository.FindById(itemId)
+	if domainErr := order.Add(item); domainErr != nil {
+		message = "Could not add Item #%i "
+		message += "to order #%i (of customer #%i) "
+		message += "as user #%i because a bussiness "
+		message += "rule was violated: '%s'"
+		err := fmt.Errorf(message,
+			item.Id,
+			order.Id,
+			order.Customer.Id,
+			user.Id,
+			domainErr.Error())
+		interactor.Logger.Log(err.Error())
+		return err
+	}
+	interactor.OrderRepository.Store(order)
+	interactor.Logger.Log(fmt.Sprintf("User added item '%s' (#%i) to order #%i",
+		item.Name,
+		item.Id,
+		order.Id))
 	return nil
 }
 

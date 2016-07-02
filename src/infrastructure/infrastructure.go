34593b060f6eb49bb7d39f31206f5cbb89acddae
kaelzhang81 <zhangy1981@qq.com>
2016-07-02 22:34:52 +0800

    add interfaces for sqlite

--- src/infrastructure/infrastructure.go
+++ src/infrastructure/infrastructure.go
@@ -1,1 +1,31 @@
 package infrastructure
+
+import (
+	"database/sql"
+	//	"fmt"
+	"interfaces"
+)
+
+type SqliteHandler struct {
+	Conn *sql.DB
+}
+
+func (handler *SqliteHandler) Execute(statement string) {
+	handler.Conn.Exec(statement)
+}
+
+func (handler *SqliteHandler) Query(statement string) interfaces.Row {
+	return nil
+}
+
+type SqliteRow struct {
+	Rows *sql.Rows
+}
+
+func (row SqliteRow) Next() bool {
+	return true
+}
+
+func (row SqliteRow) Scan(dest ...interface{}) {
+
+}
